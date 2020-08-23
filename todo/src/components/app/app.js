ac940e70a601e94cf2cc2e5f6426e7f6213d1a1d
Denys Tkachuk <ua.denist@gmail.com>
2020-08-23 20:33:19 +0300

    refactored structure

---
+++ todo/src/components/app/app.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import TodoList from '../todo-list';
+import ItemStatusFilter from '../item-status-filter';
+
+import './app.css';
+
+const App = () => {
+
+    const todoData = [
+        {label: 'Drink Coffee', important: false, id: 1},
+        {label: 'Learn React', important: true, id: 2},
+        {label: 'Build App', important: true, id: 3},
+        {label: 'Go Sleep', important: false, id: 4}
+    ]
+
+    return (
+        <div className="todo-app">
+            <AppHeader toDo={1} done={3} />
+            <div className="top-panel d-flex justify-content-between">
+                <SearchPanel />
+                <ItemStatusFilter />
+            </div>
+            <TodoList todos={todoData}/>
+        </div>
+    )
+};
+
+export default App;
--- todo/src/index.js
+++ todo/src/index.js
@@ -1,33 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list';
-import ItemStatusFilter from './components/item-status-filter';
-
-import './index.css';
-
-const App = () => {
-
-    const todoData = [
-        {label: 'Drink Coffee', important: false, id: 1},
-        {label: 'Learn React', important: true, id: 2},
-        {label: 'Build App', important: true, id: 3},
-        {label: 'Go Sleep', important: false, id: 4}
-    ]
-
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3} />
-            <div className="top-panel d-flex justify-content-between">
-                <SearchPanel />
-                <ItemStatusFilter />
-            </div>
-            <TodoList todos={todoData}/>
-        </div>
-    )
-};
+import App from './components/app';
 
 ReactDOM.render(<App />,
     document.getElementById('root'));
