28b31584210e3f277530eb6c2deb8a04f5a26133
Denys Tkachuk <ua.denist@gmail.com>
2020-08-26 00:22:19 +0300

    added filter, fixed casesensitive for search

--- todo/src/components/app/app.js
+++ todo/src/components/app/app.js
@@ -19,7 +19,8 @@ export default class App extends Component {
             this.createTodoItem('Build App'),
             this.createTodoItem('Go Sleep')
         ],
-        term: ''
+        term: '',
+        filter: 'all'
     }
 
     createTodoItem(label) {
@@ -89,24 +90,41 @@ export default class App extends Component {
         })
     }
 
+    onSearchChange = (term) => {
+        this.setState({ term });
+    }
+
+    onFilterChange = (filter) => {
+        this.setState({ filter });
+    }
+
     search(items, term) {
         if (term.length === 0) {
             return items;
         }
 
         return items.filter((item) => {
-            return item.label.indexOf(term) > -1;
+            return item.label.toLowerCase().indexOf(term.toLowerCase()) > -1;
         })
     }
 
-    onSearchChange = (term) => {
-        this.setState({ term });
+    filter(items, filter) {
+        switch (filter) {
+            case 'all':
+                return items;
+            case 'active':
+                return items.filter((item) => !item.done);
+            case 'done':
+                return items.filter((item) => item.done);
+            default:
+                return items;
+        }
     }
 
     render() {
-        const { todoData, term } = this.state;
+        const { todoData, term, filter } = this.state;
 
-        const visibleItems = this.search(todoData, term);
+        const visibleItems = this.filter(this.search(todoData, term), filter);
         const doneCount = this.state.todoData.filter((el) => el.done).length;
         const todoCount = this.state.todoData.length - doneCount;
 
@@ -115,7 +133,8 @@ export default class App extends Component {
                 <AppHeader toDo={todoCount} done={doneCount} />
                 <div className="top-panel d-flex justify-content-between">
                     <SearchPanel onSearchChange={this.onSearchChange}/>
-                    <ItemStatusFilter />
+                    <ItemStatusFilter filter={filter}
+                                      onFilterChange={this.onFilterChange}/>
                 </div>
                 <TodoList todos={ visibleItems }
                           onDeleted={ this.deleteItem }
