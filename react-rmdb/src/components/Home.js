5e392a3e9575563714a44d643c2c8c92eaf70c8c
Denys Tkachuk <ua.denist@gmail.com>
2020-08-30 19:11:01 +0300

    added items grid

--- react-rmdb/src/components/Home.js
+++ react-rmdb/src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from '../config';
 
 // import Components 
@@ -12,25 +12,44 @@ import Spinner from './elements/Spinner';
 // Custom Hook
 import { useHomeFetch } from './hooks/useHomeFetch';
 
+import NoImage from './images/no_image.jpg';
+
 const Home = () => {
-	const [{ state, loading, error }, fetchMovies] = useHomeFetch();
-	console.log(state);
+	const [
+		{
+			state: { movies, currentPage, totalPages, heroImage },
+			loading,
+			error
+		}, fetchMovies
+	] = useHomeFetch();
+	const [searchTerm, setSearchTerm] = useState('');
 
 	if (error) return <div>Something went wrong ...</div>
-	if(!state.movies[0]) return <Spinner />
+	if(!movies[0]) return <Spinner />
 
 	return (<>
 		<HeroImage
-			image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
-			title={state.heroImage.original_title}
-			text={state.heroImage.overview}/>
+			image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
+			title={heroImage.original_title}
+			text={heroImage.overview}/>
 		<SearchBar />
-		<Grid />
+		<Grid header={searchTerm ? 'Search Result' : 'Popular Movies'}>
+			{
+				movies.map(movie => (
+					<MovieThumb key={movie.id}
+								clickable
+								movieId={movie.id}
+								movieName={movie.original_title}
+								image={movie.poster_path
+									? `${IMAGE_BASE_URL}${BACKDROP_SIZE}${movie.poster_path}`
+									: NoImage } />
+				))
+			}
+		</Grid>
 		<MovieThumb />
 		<LoadMoreBtn />
 		<Spinner />
 	</>)
 }
-
 
 export default Home;
