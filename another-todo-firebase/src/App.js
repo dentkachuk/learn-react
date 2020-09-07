0a9f740a27934b95cacde70fac40f1c4dec31eeb
Denys Tkachuk <ua.denist@gmail.com>
2020-09-07 22:13:30 +0300

    start working

--- another-todo-firebase/src/App.js
+++ another-todo-firebase/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todos, setTodos] = useState(['First item', 'Second item', 'Third item']);
+	const [input, setInput] = useState('');
+
+	const addTodo = (event) => {
+		event.preventDefault();
+		setTodos([...todos, input]);
+		setInput('');
+	}
+
+	return (
+	    <div className="App">
+			<h1>Task list with Firebase 🤟</h1>
+			<form>
+				<input type="text"
+					   placeholder="Enter new todo"
+					   onChange={event => setInput(event.target.value)}
+					   value={input} />
+				<button onClick={addTodo}>Add new item</button>
+			</form>
+
+			<ul>
+				{todos.map(todo => (
+					<li>{todo}</li>
+				))}
+			</ul>
+		</div>
+    );
 }
 
 export default App;
